113a73244fc71fbf946e5d19e329ce1c68987ae7
FadiBadarni <fadybd1@gmail.com>
2024-02-05 18:43:05 +0200

    Refactor puzzle verification logic

--- server/src/controllers/boardController.js
+++ server/src/controllers/boardController.js
@@ -6,14 +6,15 @@ const generatePuzzleRoute = (app) => {
     app.get('/generate-puzzle', (req, res) => {
         try {
             const rawPuzzle = sudoku.makepuzzle();
-            const solution = sudoku.solvepuzzle(rawPuzzle);
+            const rawSolution = sudoku.solvepuzzle(rawPuzzle);
             const readablePuzzle = rawPuzzle.map(cell => cell === null ? null : cell + 1);
+            const readableSolution = rawSolution.map(cell => cell + 1);
 
             const puzzleId = generateUniqueId();
 
             puzzles[puzzleId] = {
                 puzzle: readablePuzzle,
-                solution: solution,
+                solution: readableSolution,
                 timestamp: Date.now()
             };
 
@@ -29,24 +30,37 @@ const generatePuzzleRoute = (app) => {
     });
 
     app.post('/verify-solution', (req, res) => {
-        const { puzzleId, packets } = req.body; // Packets would be the client's selection for verification
+        const { puzzleId, packet, rowIndex } = req.body;
         const puzzleData = puzzles[puzzleId];
 
         if (!puzzleData) {
             return res.status(404).send('Puzzle not found or expired');
         }
 
-        const verificationResult = verifyPackets(puzzleData.solution, packets);
+        const verificationResult = verifyPacket(puzzleData.solution, packet, rowIndex);
         res.json({ verified: verificationResult });
     });
 };
 
-function verifyPackets(solution, packets) {
-    // Implement the logic to verify the packets here.
-    // For example, check if the packets contain all numbers from 1 to 9
-    // without revealing the solution to the client.
-    return true; // Or false, based on the verification
+function verifyPacket(solution, packet, rowIndex) {
+    // Extract the row from the solution that corresponds to the selected cell.
+    let solutionRow = solution.slice(rowIndex * 9, (rowIndex + 1) * 9);
+
+    // Iterate through the user's input, checking only the filled values against the solution.
+    for (let i = 0; i < packet.length; i++) {
+        if (packet[i] === null) continue; // Skip null values which represent unfilled cells.
+
+        // Verify the filled values. Adjust for 0-indexed format as necessary.
+        if (packet[i] !== solutionRow[i] ) {
+            return false; // A discrepancy invalidates the verification.
+        }
+    }
+
+    // If the loop completes without returning false, the packet is valid.
+    return true;
 }
+
+
 
 function generateUniqueId() {
     return Math.random().toString(36).substring(2, 11);
